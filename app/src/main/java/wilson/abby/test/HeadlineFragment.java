2789149db7bb142b111e3cc83564c6cb8820a02d
abby wilson <abbyw42@gmail.com>
2017-06-13 18:36:23 +0100

    Rebase of fragments

---
+++ app/src/main/java/wilson/abby/test/HeadlineFragment.java
@@ -0,0 +1,45 @@
+package wilson.abby.test;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.ListFragment;
+import android.widget.ListView;
+
+public class HeadlineFragment extends ListFragment {
+    private OnHeadlineSelectedListener mCallback;
+
+    public interface OnHeadlineSelectedListener {
+        void onArticleSelected(int position);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.headline_fragment, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (OnHeadlineSelectedListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement OnHeadlineSelectedListener");
+        }
+    }
+
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // Send the event to the host activity
+        mCallback.onArticleSelected(position);
+    }
+
+
+}
--- app/src/main/java/wilson/abby/test/MainActivity.java
+++ app/src/main/java/wilson/abby/test/MainActivity.java
@@ -2,15 +2,14 @@ package wilson.abby.test;
 
 import android.content.Intent;
 import android.os.Build;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HeadlineFragment.OnHeadlineSelectedListener {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     private int textView;
 
@@ -18,9 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TextView textView = new TextView(this);
-        //textView.setText(R.string.hello_world);
 
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            android.support.v4.app.Fragment firstFragment = new android.support.v4.app.Fragment();
+            ArticleFragment af = new ArticleFragment();
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            // Add the fragment to the 'fragment_container' FrameLayout
+            ft.add(R.id.fragment_container, firstFragment);
+            ft.replace(R.id.fragment_container, af);
+            ft.commit();
+        }
     }
 
     /** Called when the user taps the Send button */
@@ -32,11 +52,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void setUpActionBar() {
-        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            android.app.ActionBar actionBar = getActionBar();
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+    public void onArticleSelected(int position) {
+        // when the headline fragment is clicked, replace the fragment in the fragment container with a new
+        // headline fragment, to show a change
+            HeadlineFragment hf = new HeadlineFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, hf).commit();
     }
 }
