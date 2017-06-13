d42491be2228f9d5f1ff5ef7f86866539cde011a
abby wilson <abbyw42@gmail.com>
2017-06-13 18:31:18 +0100

    Platform Support

--- app/src/main/java/wilson/abby/test/MainActivity.java
+++ app/src/main/java/wilson/abby/test/MainActivity.java
@@ -1,6 +1,8 @@
 package wilson.abby.test;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,5 +30,13 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    private void setUpActionBar() {
+        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            android.app.ActionBar actionBar = getActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
